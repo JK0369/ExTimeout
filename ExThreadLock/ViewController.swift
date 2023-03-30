4a14136920e645a2ad01cf0fbf1dd85c84c7d67a
jake <palatable7@naver.com>
2023-03-31 02:36:07 +0900

    add

--- ExThreadLock/ViewController.swift
+++ ExThreadLock/ViewController.swift
@@ -16,23 +16,25 @@ class ViewController: UIViewController {
         let workItem = DispatchWorkItem { [weak self] in
             print("start")
             let value = self?.someDelayWork()
-            print("end")
+            print("end", value)
         }
         
         queue.async(execute: workItem)
         print("test>")
-        let result = workItem.wait(timeout: DispatchTime.now() + 10)
-        
+        let result = workItem.wait(timeout: DispatchTime.now() + 1)
+
         switch result {
         case .success:
             print("success")
         case .timedOut:
+            workItem.cancel()
+            print("isCancelled?", workItem.isCancelled)
             print("timedOut")
         }
     }
     
     private func someDelayWork() -> Int {
-        sleep(10)
+        sleep(3)
         return 3
     }
 }
